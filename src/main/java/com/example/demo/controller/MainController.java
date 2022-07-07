5a37b2b48d0e40244aba3251c2f8307096fc0b59
lindn <lindner634@gmail.com>
2022-07-07 05:51:19 +0200

    init

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,2 +1,13 @@
-package com.example.demo.controller;public class MainController {
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String start(){
+        return "index";
+    }
 }
